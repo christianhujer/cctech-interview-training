d2df08ef066ca21dc6ae0c0a7b15c4b9dcecaa84
unknown <komalsuryawanshi92@gmail.com>
2022-06-28 14:21:17 +0530

    Add Palindrome

---
+++ palindrome/Palindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+bool isPalindrome(string palindromeCandidate);
+int main(int argc, char *argv[] )
+{
+    
+    
+    for(int i=0;i<argc;i++)
+    {
+        isPalindrome(argv[1]);
+    }
+
+    return 0;
+}
+
+bool isPalindrome(string palindromeCandidate)
+{
+
+    for(int i=0;i < palindromeCandidate.length(); i++)
+    {
+       
+        if(palindromeCandidate[i] != palindromeCandidate[palindromeCandidate.length()-i-1])
+            return EXIT_SUCCESS;
+          
+        else 
+            return EXIT_FAILURE;   
+                
+    }
+}
+    
