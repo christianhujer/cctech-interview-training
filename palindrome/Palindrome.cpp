dfc15350ce363f3cf384b6bcb8efc364e7de9e96
komalsuryawanshi92 <104582247+komalsuryawanshi92@users.noreply.github.com>
2022-06-28 16:02:43 +0530

    Update Palindrome.cpp

--- palindrome/Palindrome.cpp
+++ palindrome/Palindrome.cpp
@@ -15,17 +15,18 @@ int main(int argc, char *argv[] )
     return 0;
 }
 
-bool isPalindrome(string palindromeCandidate)
+bool isPalindrome(string palindromeCandidate) 
 {
 
     for(int i=0;i < palindromeCandidate.length(); i++)
     {
        
-        if(palindromeCandidate[i] != palindromeCandidate[palindromeCandidate.length()-i-1])
-            return EXIT_SUCCESS;
+        if(palindromeCandidate[i] != palindromeCandidate[palindromeCandidate.length()-i-1])// // check string not equal to (palindrome-i-1)
+            
+            return EXIT_SUCCESS; //true 
           
         else 
-            return EXIT_FAILURE;   
+            return EXIT_FAILURE; // false
                 
     }
 }
